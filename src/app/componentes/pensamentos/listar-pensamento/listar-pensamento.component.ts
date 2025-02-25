bae652c0d03389de4d28099d1b4469498f71fd2a
Henriquehlr <henrique-22@hotmail.com>
2025-02-25 16:26:46 -0300

    Feat: Alimentando componentes

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  
+  listaPensamentos = [
+    {
+      conteudo: 'Passe informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com o @input',
+      autoria: 'Angular',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @inputMinha propriedade é decorada com o @input',
+      autoria: 'Angular',
+      modelo: 'modelo1',
+    },
+  ];
+  
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
