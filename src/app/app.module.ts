655dae17383ad544322696d884032f2fda681eca
Henriquehlr <henrique-22@hotmail.com>
2025-02-25 12:54:37 -0300

    Feat: Acões Completas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamemtoComponent } from './componentes/pensamentos/criar-pensamemto/criar-pensamemto.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CriarPensamemtoComponent } from './componentes/pensamentos/criar-pensam
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/pensamentos/criar-pensamemto/criar-pensamemto.component.ts
+++ src/app/componentes/pensamentos/criar-pensamemto/criar-pensamemto.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamemtoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'Modelo 1'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert("teste")
+  }
+
+  cancelarPensamento() {
+    alert("Cancelado")
   }
 
 }
