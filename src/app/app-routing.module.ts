746869e8c1bef2acd7ad5cb757f28d8ecd6b0265
Henriquehlr <henrique-22@hotmail.com>
2025-02-26 14:24:34 -0300

    FEAT: Execução botão excluir pensamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamemtoComponent } from './componentes/pensamentos/criar-pensamemto/criar-pensamemto.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'listarPensamento',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'criarPensamento',
-    component: CriarPensamemtoComponent
+    component: CriarPensamemtoComponent,
   },
   {
     path: 'listarPensamento',
-    component: ListarPensamentoComponent
-  }
+    component: ListarPensamentoComponent,
+  },
+  {
+    path: 'pensamentos/excluirPensamento/:id',
+    component: ExcluirPensamentoComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
